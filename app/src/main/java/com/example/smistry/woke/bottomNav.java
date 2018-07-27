2476c0ae18335ef01aa8ca376d070b84f80784cf
Suchita Mistry <smistry@fb.com>
2018-07-27 15:58:17 -0700

    Weather api working

--- app/src/main/java/com/example/smistry/woke/bottomNav.java
+++ app/src/main/java/com/example/smistry/woke/bottomNav.java
@@ -3,13 +3,11 @@ package com.example.smistry.woke;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -24,7 +22,6 @@ import com.example.smistry.woke.models.Task;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
-import org.parceler.Parcels;
 
 import java.io.File;
 import java.sql.Time;
