259fe6f0a70fcfe2b4677dc94887b561c161ec1d
Suchita Mistry <smistry@fb.com>
2018-08-06 16:41:17 -0700

    Added splash screen gif

---
+++ app/src/main/java/com/example/smistry/woke/SplashScreen.java
@@ -0,0 +1,35 @@
+package com.example.smistry.woke;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(2500);   // set the duration of splash screen
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, bottomNav.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
